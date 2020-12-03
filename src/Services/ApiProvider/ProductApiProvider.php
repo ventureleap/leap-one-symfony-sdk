aad2fda50c80c21436a90e19084b6b8dfe70e91b
Fernando Escalante <escalantef@outlook.com>
2020-12-03 13:59:46 +0100

    Apis added

--- src/Services/ApiProvider/ProductApiProvider.php
+++ src/Services/ApiProvider/ProductApiProvider.php
@@ -5,7 +5,9 @@ namespace VentureLeap\LeapOnePhpSdk\Services\ApiProvider;
 
 
 use VentureLeap\ProductService\Api\ConfigurationEntryApi;
+use VentureLeap\ProductService\Api\PriceListProductApi;
 use VentureLeap\ProductService\Api\ProductApi;
+use VentureLeap\ProductService\Api\ProductCategoryApi;
 use VentureLeap\ProductService\Configuration;
 
 class ProductApiProvider extends AbstractLeapOneApiProvider
@@ -19,5 +21,15 @@ class ProductApiProvider extends AbstractLeapOneApiProvider
     public function getProductApi(): ProductApi
     {
         return new ProductApi(null, $this->getConfiguration());
+    }
+
+    public function getProductCategoryApi(): ProductCategoryApi
+    {
+        return new ProductCategoryApi(null, $this->getConfiguration());
+    }
+
+    public function getPriceListProductApi(): PriceListProductApi
+    {
+        return new PriceListProductApi(null, $this->getConfiguration());
     }
 }
