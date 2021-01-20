1fbb2ed6fe449d2baf956d05e95dd662947108e9
damilare <damilare.durojaye@gmail.com>
2021-01-20 13:12:31 +0100

    L1-3476: Added translation file

--- src/Form/Type/UserPasswordRequestType.php
+++ src/Form/Type/UserPasswordRequestType.php
@@ -5,6 +5,7 @@ namespace VentureLeap\LeapOnePhpSdk\Form\Type;
 
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -17,7 +18,7 @@ class UserPasswordRequestType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', TextType::class)
+            ->add('email', EmailType::class)
             ->add('submit', SubmitType::class);
     }
 
