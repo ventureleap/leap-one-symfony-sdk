36c725a66a0a2f22b374c5472a97585c31bd474f
Szilard Stadler <szilard.stadler@venture-leap.com>
2021-03-05 14:03:21 +0100

    Filters for Renderer API

---
+++ src/Services/ApiProvider/Client/Renderer/Render.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace VentureLeap\LeapOneSymfonySdk\Services\ApiProvider\Client\Renderer;
+
+use VentureLeap\LeapOneSymfonySdk\Services\ApiProvider\CollectionFilter\Renderer\RenderFilter;
+use VentureLeap\RendererService\Api\RenderApi;
+
+class Render extends RenderApi
+{
+    public function getFilteredCollectionWithHttpInfo(RenderFilter $filter)
+    {
+        $this->getRenderCollectionWithHttpInfo(
+            $filter->getProperties(),
+            $filter->getCustomData(),
+            $filter->getFileName(),
+            $filter->getFileType(),
+            $filter->getCreatedAtBefore(),
+            $filter->getCreatedAtStrictlyBefore(),
+            $filter->getCreatedAtAfter(),
+            $filter->getCreatedAtStrictlyAfter(),
+            $filter->getUpdatedAtBefore(),
+            $filter->getUpdatedAtStrictlyBefore(),
+            $filter->getUpdatedAtAfter(),
+            $filter->getUpdatedAtStrictlyAfter(),
+            $filter->getPage()
+        );
+    }
+
+    public function getFilteredCollection(RenderFilter $filter)
+    {
+        $this->getRenderCollection(
+            $filter->getProperties(),
+            $filter->getCustomData(),
+            $filter->getFileName(),
+            $filter->getFileType(),
+            $filter->getCreatedAtAfter(),
+            $filter->getCreatedAtStrictlyBefore(),
+            $filter->getCreatedAtAfter(),
+            $filter->getCreatedAtStrictlyAfter(),
+            $filter->getUpdatedAtAfter(),
+            $filter->getUpdatedAtStrictlyBefore(),
+            $filter->getUpdatedAtAfter(),
+            $filter->getUpdatedAtStrictlyAfter(),
+            $filter->getPage()
+        );
+    }
+
+}
---
+++ src/Services/ApiProvider/CollectionFilter/Renderer/TemplateFilter.php
@@ -0,0 +1,163 @@
+<?php
+
+
+namespace VentureLeap\LeapOneSymfonySdk\Services\ApiProvider\CollectionFilter\Renderer;
+
+
+class TemplateFilter
+{
+    private ?array $properties;
+    private ?string $customData;
+    private ?string $fileName;
+    private ?string $fileType;
+    private ?string $templateKey;
+    private ?string $createdAtBefore;
+    private ?string $createdAtStrictlyBefore;
+    private ?string $createdAtAfter;
+    private ?string $createdAtStrictlyAfter;
+    private ?string $updatedAtBefore;
+    private ?string $updatedAtStrictlyBefore;
+    private ?string $updatedAtAfter;
+    private ?string $updatedAtStrictlyAfter;
+    private int $page = 1;
+
+    public function getProperties(): ?array
+    {
+        return $this->properties;
+    }
+
+    public function setProperties(?array $properties): void
+    {
+        $this->properties = $properties;
+    }
+
+    public function getCustomData(): ?string
+    {
+        return $this->customData;
+    }
+
+    public function setCustomData(?string $customData): void
+    {
+        $this->customData = $customData;
+    }
+
+    public function getFileName(): ?string
+    {
+        return $this->fileName;
+    }
+
+    public function setFileName(?string $fileName): void
+    {
+        $this->fileName = $fileName;
+    }
+
+    public function getFileType(): ?string
+    {
+        return $this->fileType;
+    }
+
+    public function setFileType(?string $fileType): void
+    {
+        $this->fileType = $fileType;
+    }
+
+    public function getTemplateKey(): ?string
+    {
+        return $this->templateKey;
+    }
+
+    public function setTemplateKey(?string $templateKey): void
+    {
+        $this->templateKey = $templateKey;
+    }
+
+    public function getCreatedAtBefore(): ?string
+    {
+        return $this->createdAtBefore;
+    }
+
+    public function setCreatedAtBefore(?string $createdAtBefore): void
+    {
+        $this->createdAtBefore = $createdAtBefore;
+    }
+
+    public function getCreatedAtStrictlyBefore(): ?string
+    {
+        return $this->createdAtStrictlyBefore;
+    }
+
+    public function setCreatedAtStrictlyBefore(?string $createdAtStrictlyBefore): void
+    {
+        $this->createdAtStrictlyBefore = $createdAtStrictlyBefore;
+    }
+
+    public function getCreatedAtAfter(): ?string
+    {
+        return $this->createdAtAfter;
+    }
+
+    public function setCreatedAtAfter(?string $createdAtAfter): void
+    {
+        $this->createdAtAfter = $createdAtAfter;
+    }
+
+    public function getCreatedAtStrictlyAfter(): ?string
+    {
+        return $this->createdAtStrictlyAfter;
+    }
+
+    public function setCreatedAtStrictlyAfter(?string $createdAtStrictlyAfter): void
+    {
+        $this->createdAtStrictlyAfter = $createdAtStrictlyAfter;
+    }
+
+    public function getUpdatedAtBefore(): ?string
+    {
+        return $this->updatedAtBefore;
+    }
+
+    public function setUpdatedAtBefore(?string $updatedAtBefore): void
+    {
+        $this->updatedAtBefore = $updatedAtBefore;
+    }
+
+    public function getUpdatedAtStrictlyBefore(): ?string
+    {
+        return $this->updatedAtStrictlyBefore;
+    }
+
+    public function setUpdatedAtStrictlyBefore(?string $updatedAtStrictlyBefore): void
+    {
+        $this->updatedAtStrictlyBefore = $updatedAtStrictlyBefore;
+    }
+
+    public function getUpdatedAtAfter(): ?string
+    {
+        return $this->updatedAtAfter;
+    }
+
+    public function setUpdatedAtAfter(?string $updatedAtAfter): void
+    {
+        $this->updatedAtAfter = $updatedAtAfter;
+    }
+
+    public function getUpdatedAtStrictlyAfter(): ?string
+    {
+        return $this->updatedAtStrictlyAfter;
+    }
+
+    public function setUpdatedAtStrictlyAfter(?string $updatedAtStrictlyAfter): void
+    {
+        $this->updatedAtStrictlyAfter = $updatedAtStrictlyAfter;
+    }
+
+    public function getPage(): int
+    {
+        return $this->page;
+    }
+
+    public function setPage(int $page): void
+    {
+        $this->page = $page;
+    }
+}
