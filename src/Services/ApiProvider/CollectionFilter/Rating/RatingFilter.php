1a497f759c86df8f15bab5cb6e00c2a2d506992f
Szilard Stadler <szilard.stadler@venture-leap.com>
2021-03-05 13:37:10 +0100

    Filters for Rating API

---
+++ src/Services/ApiProvider/CollectionFilter/Rating/RatingFilter.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace VentureLeap\LeapOneSymfonySdk\Services\ApiProvider\CollectionFilter\Rating;
+
+
+class RatingFilter
+{
+    private ?string $productUuid;
+    private ?bool $existsReview;
+    private ?string $customData;
+    private int $page = 1;
+
+    public function getProductUuid(): ?string
+    {
+        return $this->productUuid;
+    }
+
+    public function setProductUuid(?string $productUuid): void
+    {
+        $this->productUuid = $productUuid;
+    }
+
+    public function getExistsReview(): ?bool
+    {
+        return $this->existsReview;
+    }
+
+    public function setExistsReview(?bool $existsReview): void
+    {
+        $this->existsReview = $existsReview;
+    }
+
+    public function getCustomData(): ?string
+    {
+        return $this->customData;
+    }
+
+    public function setCustomData(?string $customData): void
+    {
+        $this->customData = $customData;
+    }
+
+    public function getPage(): int
+    {
+        return $this->page;
+    }
+
+    public function setPage(int $page): void
+    {
+        $this->page = $page;
+    }
+
+}
